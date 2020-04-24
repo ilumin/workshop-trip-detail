bffc44ab1908577328c6f22dbc89b4715434e535
ilumin <iolumin@gmail.com>
2020-04-24 17:36:10 +0700

    add simple layout

--- index.js
+++ index.js
@@ -7,9 +7,42 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <div className="card">
+
+          <div className="header">
+            <div className="header-content">
+              <h2 className="location" id="location-name">Berlin</h2>
+              <div className="route-status" id="route-status">in route</div>
+              <div className="day-counter">
+                <span className="badge">4</span>
+                day trip
+              </div>
+            </div>
+            <div className="header-bg"></div>
+            <a href="#" className="button-join" id="button-join">join</a>
+          </div>
+
+          <div className="body">
+            <div className="next-trip">
+              <span className="label">Next trip</span>
+              <a href="#" className="from">Berlin</a>
+              <a href="#" className="to">Prague</a>
+            </div>
+            <div className="content">
+              <p>asdasdasdasdasd</p>
+            </div>
+            <ul className="liked-profiles">
+              <li><a href="#" className="profile"><img className="avatar" /></a></li>
+              <li><a href="#" className="view-more" id="view-more">+2</a></li>
+            </ul>
+            <ul className="tools">
+              <li><a href="#" className="tool tool-like" id="tool-like">like</a></li>
+              <li><a href="#" className="tool tool-share" id="tool-share">share</a></li>
+              <li><a href="#" className="tool tool-save" id="tool-save">save</a></li>
+            </ul>
+          </div>
+
+        </div>
       </Wrapper>
     );
   }
--- index.styles.js
+++ index.styles.js
@@ -1,5 +1,107 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  padding: 20px;
+  background: yellow;
+  font-size: 16px;
+
+  .card {
+    position: relative;
+    background: #ffffff;
+    border-radius: 1em;
+
+    .header {
+      position: relative;
+      height: 100px;
+
+      .header-content {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: #DDD;
+
+        .location {
+          position: absolute;
+          bottom: 1em;
+          left: 1em;
+
+          margin: 0;
+          font-size: 1em;
+        }
+        .route-status {
+          position: absolute;
+          bottom: 1em;
+          right: 1em;
+          
+          font-size: 1em;
+        }
+        .day-counter {
+          position: absolute;
+          top: 1em;
+          left: 1em;
+          
+          font-size: 1em;
+        }
+      }
+
+      .header-bg {}
+
+      .button-join {
+        position: absolute;
+        bottom: -17px;
+        left: 50%;
+        box-sizing: border-box;
+        width: 100px;
+        height: 34px;
+
+        margin-left: -50px;
+
+        text-align: center;
+        background: yellow;
+        padding: .5em;
+        border-radius: 1em;
+      }
+    }
+
+    .body {
+      padding: 1em;
+
+      .next-trip {
+        margin: 1em 0;
+      }
+      .content {}
+
+      ul {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+
+        li {
+          display: inline;
+        }
+      }
+
+      .like-profiles {}
+      .tools {
+        border-top: solid 1px #ccc;
+        padding-top: .5em;
+        margin-top: .5em;
+        height: 30px;
+
+        .tool {
+          float: left;
+          width: 30px;
+          height: 30px;
+          background: #000;
+          margin: 0 4px;
+        }
+
+        .tool-save {
+          float: right;
+        }
+      }
+    }
+  }
+`
 
 export default Wrapper
